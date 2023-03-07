3a086e0ce9cc7cd243ca49757ba8f8ad462d996e
DaBeRodriguez <121107391+DaBeRodriguez@users.noreply.github.com>
2023-03-07 10:30:45 -0500

    forEach

--- unidad4/firebase/crud/main.js
+++ unidad4/firebase/crud/main.js
@@ -1,21 +1,34 @@
-import { GuardarTarea , ListarTareas } from "./app/firebase.js"
-window.addEventListener("DOMContentLoaded",   () => { 
-    ListarTareas() ;
-    // console.log (lista);   
-    // lista.forEach((doc) => {
-    //     console.log (doc);
-        
-    // });
-   
+import { GuardarTarea, ListarTareas } from "./app/firebase.js";
+
+let contenedor = document.getElementById("contenedor");
+
+window.addEventListener("DOMContentLoaded", async () => {
+  let lista = await ListarTareas();
+  // console.log(lista);
+  lista.forEach((doc) => {
+    console.log(doc.data()._titulo);
+
+    let tarea = doc.data()._titulo;
+    let html = `
+      <div>
+        <h3 id='aaa'></h3>
+      </div>
+    `
+
+    
+    contenedor.innerHTML = tarea;
+  });
 });
-let formulario = document.getElementById("formulario")
+
+let formulario = document.getElementById("formulario");
+
 formulario.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let titulo = formulario["titulo"].value;
-    let descripcion = formulario["descripcion"].value;
-    // console.log (titulo,descripcion);
-    GuardarTarea(titulo, descripcion);
-    formulario.reset(); //limpiamos el formulario 
+  e.preventDefault(); //Evita que recargue la pagina
+  let titulo = formulario["titulo"].value;
+  let descripcion = formulario["descripcion"].value;
+  // console.log(titulo, descripcion);
 
+  GuardarTarea(titulo, descripcion);
 
-})
+  formulario.reset(); //limpiamos el formulario
+});
